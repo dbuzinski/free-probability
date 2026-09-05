10edbc3765ece2d219f1c715b91ec0d070c411b5
David Buzinski <davidbuzinski@gmail.com>
2026-09-05 17:30:58 -0400

    Initial commit

---
+++ FreeProbability.lean
@@ -0,0 +1,1 @@
+import FreeProbability.Basic
---
+++ FreeProbability/Basic.lean
@@ -0,0 +1,21 @@
+import Mathlib
+
+/-
+Copyright (c) 2026 David Buzinski. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Buzinski
+-/
+
+/-!
+# Free Probability
+
+Foundational definitions for a formalization of *Lectures on the
+Combinatorics of Free Probability*.
+-/
+
+namespace FreeProbability
+
+example (n : ℕ) : n + 0 = n := by
+  simp
+
+end FreeProbability
